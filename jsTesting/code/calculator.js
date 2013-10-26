fa9d62860009098041141f9a82c1b7f41dd93aaa
Ward Bell <wardbell@hotmail.com>
2013-10-25 19:52:18 -0700

    jasmine setTimeout (async and mocked) tests

--- jsTesting/code/calculator.js
+++ jsTesting/code/calculator.js
@@ -4,6 +4,7 @@
     /* Application code here */
 
     // Joe Eames: "Testing Clientside JavaScript: async tests"
+    // http://pluralsight.com/training/Player?author=joe-eames&name=testing-javascript-m7-utilities&mode=live&clip=0&course=testing-javascript
 
     code.Calculator = Calculator;
 
@@ -11,9 +12,17 @@
         this.$el = $(displayElement); // wrap as jQuery element
     }
 
+    Calculator.HIDE_RESULT_MS = 500;
+    Calculator.PAUSE_MS = 1000;
+
     // Fadeout effect on the result
     Calculator.prototype.hideResult = function (callback) {
-        this.$el.fadeOut(500, callback);
+        this.$el.fadeOut(Calculator.HIDE_RESULT_MS, callback);
+    };
+
+    // pause before hiding
+    Calculator.prototype.pause = function (callback) {
+        setTimeout(callback, Calculator.PAUSE_MS);
     };
 
 
--- jsTesting/tests/Jasmine/calculatorAsyncSpec.js
+++ jsTesting/tests/Jasmine/calculatorAsyncSpec.js
@@ -1,6 +1,12 @@
 ﻿(function(testFns, code) {
     "use strict";
 
+    var Calculator = code.Calculator;
+
+    // jasmine-async stuff ... we'll explain below
+    var async = new AsyncSpec(this);
+    var xasync = testFns.xasync; // to disable async tests with 'x' prefix
+
     describe("Calculator", function () {
         var calc;
 
@@ -9,9 +15,10 @@
 
             beforeEach(function () {
 
-                el = $("<div>the results</div>");
+                el = $('<style>#container {visibility:hidden}</style>'+
+                       '<div id="container">the results</div>');
                 $('body').append(el);
-                calc = new code.Calculator(el); 
+                calc = new Calculator(el); 
 
             });
 
@@ -22,71 +29,121 @@
 
             /*** TESTS ***/
 
-            /*---- oops .. no async awareness -----*/
+            describe("hide result test", function () {
+                /*---- oops .. no async awareness -----*/
 
-            xit("should hide result <doesn't work>", function () {
+                xit("should fail w/ non-async test", function () {
 
-                calc.hideResult(callback);
-
-                function callback() {
-                    // not actually called within this tests timeframe!
-                    // it will infect a later-running test!
-                    expect(el.css("display")).toBe("none");
-                }
-
-                // won't work called here because it hasn't happened in the DOM yet
-                expect(el.css("display")).toBe("none");
-            });
-
-            /*---- runs/waitsFor -----*/
-
-            // native jasmine style
-
-            it("should hide result <with runs/waitsFor>", function () {
-
-                var flag; // waitsFor
-
-                runs(function () {
                     calc.hideResult(callback);
-                });
-                
-                waitsFor(function () {
-                    return flag;
-                },
-                "hide result flag to be true", /* timeout error msg */
-                600 /*Timeout value ... 100ms longer than the fadeout. Fails at 100.*/                );
 
-                runs(function () {
+                    function callback() {
+                        // not actually called within this tests timeframe!
+                        // it will infect a later-running test!
+                        expect(el.css("display")).toBe("none");
+                    }
+
+                    // won't work called here because it hasn't happened in the DOM yet
                     expect(el.css("display")).toBe("none");
                 });
 
-                function callback() {
-                    flag = true;
-                }
+                /*---- runs/waitsFor -----*/
 
+                // native jasmine style
+
+                it("should work with runs/waitsFor", function () {
+
+                    var flag; // waitsFor
+
+                    runs(function () {
+                        calc.hideResult(callback);
+                    });
+
+                    waitsFor(function () {
+                        return flag;
+                    },
+                    "hide result flag to be true", /* timeout error msg */
+                    600 /*Timeout value ... 100ms longer than the fadeout. Fails at 100.*/);
+
+                    runs(function () {
+                        expect(el.css("display")).toBe("none");
+                    });
+
+                    function callback() {
+                        flag = true;
+                    }
+
+                });
+
+
+                /*---- Derick's jasmine-async 'async.it' ----*/
+
+                // Mocha.js style
+
+                async.it("should work with async.it", function (done) {
+
+                    calc.hideResult(callback);
+
+                    function callback() {
+                        expect(el.css("display")).toBe("none");
+                        done();
+                    }
+                });
             });
 
 
-            /*---- Derick's async.it ----*/
 
-            // Mocha.js style
 
-            // These belong at the top of first describe
-            var async = new AsyncSpec(this); 
-            var xasync = testFns.xasync;     // to disable async tests with 'x' prefix
+            describe("pause before hiding test", function () {
 
-            async.it("should hide result <with async.it>", function (done) {
-                calc.hideResult(callback);
+                var pauseTime = Calculator.PAUSE_MS + "ms";
+                async.it("should pause for "+pauseTime, function (done) {
 
-                function callback() {
-                    expect(el.css("display")).toBe("none");
-                    done();
-                }
+                    // No timeout in jasmine-async so always get Jasmine default of 5 sec
+                    // Can change that for a given spec with this
+                    // jasmine.getEnv().currentSpec.env.defaultTimeoutInterval = time-in-ms
+
+                    // Timeout if significantly exceeds expected time.
+                    jasmine.getEnv().currentSpec.env
+                           .defaultTimeoutInterval = Calculator.PAUSE_MS + 100;
+
+                    var donePausing = false;
+                    calc.pause(callback);
+
+                    function callback() {
+                        var donePausing = true;
+                        // ... stuff happens
+                        done();
+                    }
+                });
             });
+
+            describe("pause before hiding test with jasmine mock clock", function () {
+
+                beforeEach(function () {
+                    jasmine.Clock.useMock();
+                });
+
+                it("should mock the pause (sync test)", function () {
+
+                    var donePausing = false;
+                    calc.pause(callback);
+
+                    // move time ahead 1ms short
+                    jasmine.Clock.tick(Calculator.PAUSE_MS - 1 ); 
+                    expect(donePausing).toBeFalsy(); // still not called
+
+                    // move time ahead the last ms.
+                    jasmine.Clock.tick(100); 
+                    expect(donePausing).toBeTruthy(); // called now
+
+                    function callback() {
+                        donePausing = true;
+                    }
+                });
+            });
+
+
         });
-
-
-
     });
 
 
